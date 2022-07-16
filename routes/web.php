0161f04c0f335e48b222d65ef2be82138760b76a
adilovua <u_adilov@mail.ru>
2022-07-16 20:33:44 +0500

    route tasks 4

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,23 @@ Route::get('/user/{id}', function($id){
     return "Your ID is $id";
 })->where('id', '[0-9]+');
 
+/**
+ * Сделайте маршрут вида /user/:id/:name, где вместо :id должно быть число,
+ * а вместо :name - строка, состоящая из маленьких латинских букв количеством более 2-х.
+ */
+Route::get('/user/{id}/{name}', function ($id, $name){
+    return "Hey $name! Your ID id $id";
+})->where('id', '[0-9]+')->where('name', '[a-z][a-z]');
+
+/**
+ * Сделайте маршрут вида /posts/:date,
+ * где вместо :date должна быть дата в формате год-месяц-день.
+ */
+
+Route::get('/post/{date}', function($date){
+    return "Date is $date";
+})->where('date', '(?:0[1-9]|[12][0-9]|3[01])[-/.](?:0[1-9]|1[012])[-/.](?:19\d{2}|20[01][0-9]|2020)\b');
+
 Route::get('/{surname}/{name}', function($surname, $name){
     switch ($surname) {
         case "city":
