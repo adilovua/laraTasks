6074825cbc554369713121a1170056b432af91a9
adilovua <u_adilov@mail.ru>
2022-07-16 12:55:50 +0500

    tasks with routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,22 @@ Route::get('/', function () {
 Route::get('/test', function(){
     return '<h1>This is the test page</h1>';
 });
+
+Route::get('/dir/test', function(){
+    return '<h1>This is the test page on URL->/dir/test</h1>';
+});
+
+Route::get('/user/{name}', function($name){
+    return "Your name is $name";
+});
+
+Route::get('/{surname}/{name}', function($surname, $name){
+    if ($surname!='city') {
+        return "Your name is $surname $name";}
+    else
+    {
+        $name="Tashkent";
+        return "Cityname is: $name";
+    }
+});
+
