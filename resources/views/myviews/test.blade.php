7dc09aefae3171f5c01ba9d56cacb01272a7fb0a
adilovua <u_adilov@mail.ru>
2022-07-17 23:59:41 +0500

    123456

--- resources/views/myviews/test.blade.php
+++ resources/views/myviews/test.blade.php
@@ -3,7 +3,7 @@
 		<p style="{{ $style }}"> {{ $name }} </p>
 		<p> {!! $age !!}  years old. </p>
 		<p> His salary is {{ $salary }}. </p>
-
+{{--This os comment--}}
 		<p><input type="tex" value="{{ $name }}"/></p>
 		<p><input type="tex" value="{{ $age }}"/></p>
 		<p><input type="tex" value="{{ $salary }}"/></p>
