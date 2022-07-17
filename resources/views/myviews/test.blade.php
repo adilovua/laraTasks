a814a5cac5fb6df01225633c51133bdd39f619b5
adilovua <u_adilov@mail.ru>
2022-07-17 14:24:27 +0500

    components in views

--- resources/views/myviews/test.blade.php
+++ resources/views/myviews/test.blade.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>my view</title>
-	</head>
-	<body>
-        Test view
-
-		<p> variable one: {{ $surname }} </p>
-		<p>	variable two: {{ $name }} </p>
-    </body>
-</html>
+<x-layout>
+	page content
+</x-layout>
